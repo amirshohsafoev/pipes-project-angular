8d4cdfe7c6a53d363eb2786bd97a0bcbcd65970c
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-21 18:50:47 -0400

    creating own pipes, async pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
